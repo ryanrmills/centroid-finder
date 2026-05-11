9a2b5f3fe6f90599c421ce6929e49149e8183b2e
Ryan Mills <ryanroymills@gmail.com>
2026-05-10 22:28:06 -0700

    finished wave 2

--- src/DfsBinaryGroupFinder.java
+++ src/DfsBinaryGroupFinder.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class DfsBinaryGroupFinder implements BinaryGroupFinder {
    /**
@@ -30,7 +33,79 @@ public class DfsBinaryGroupFinder implements BinaryGroupFinder {
     */
     @Override
     public List<Group> findConnectedGroups(int[][] image) {
-        return null;
+        //have a set of visited
+        Set<String> visited = new HashSet<>();
+        List<Group> groupList = new ArrayList<>();
+        
+        for (int r = 0; r < image.length; r++){
+            for (int c = 0; c < image[r].length; c++){
+                String coordString = "" + c + " " + r;
+                if (image[r][c] == 1 && !visited.contains(coordString)){
+                    List<Integer> stats = new ArrayList<>();
+                    stats.add(0);
+                    stats.add(0);
+                    stats.add(0);
+
+                    stats = findGroupsHelper(
+                        stats,
+                        image, 
+                        visited,
+                        coordString, 
+                        new Coordinate(c, r)
+                    );
+                    int groupSize = stats.get(0);
+                    int xAvg = stats.get(1) / groupSize;
+                    int yAvg = stats.get(2) / groupSize;
+                    groupList.add(new Group(groupSize, new Coordinate(xAvg, yAvg)));
+                }
+            }
+        }
+        
+        groupList.sort(null);
+        return groupList;
     }
-    
+
+    //helper method
+    public List<Integer> findGroupsHelper(
+        List<Integer> stats,
+        int[][] image, 
+        Set<String> visited,
+        String coordString,
+        Coordinate coordinate
+    ){
+        if (visited.contains(coordString)){
+            return stats;
+        }
+
+        visited.add(coordString);
+        stats.set(0, stats.get(0) + 1);
+        stats.set(1, stats.get(1) + coordinate.x());
+        stats.set(2, stats.get(2) + coordinate.y());
+
+        //coords
+        int[][] coords = {
+            {-1, 0},
+            {1, 0},
+            {0, -1},
+            {0, 1}
+        };
+
+        for (int[] coord : coords){
+            //not less than image length && not greater than or equal to image length
+            //not less than image row length && not greater than or equal to image row length
+            int xCoord = coordinate.x() + coord[1];
+            int yCoord = coordinate.y() + coord[0];
+            String newCoordString = "" + xCoord + " " + yCoord;
+            if (
+                yCoord >= 0 && yCoord < image.length &&
+                xCoord >= 0 && xCoord < image[coordinate.y()].length &&
+                image[yCoord][xCoord] == 1 &&
+                !visited.contains(newCoordString)
+            ){
+                stats = findGroupsHelper(stats, image, visited, newCoordString, new Coordinate(xCoord, yCoord));
+            }
+        }
+
+        return stats;
+    }
 }
